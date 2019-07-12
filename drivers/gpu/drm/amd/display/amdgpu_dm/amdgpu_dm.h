3b9bfa511371a1a13250687b3722af4c3d1c96e9
changzhu <Changfeng.Zhu@amd.com>
2019-07-12 14:49:19 +0800

    drm/amdkcl: add AC_AMDGPU_DRM_AUDIO_COMPONENT_HEADER
    it is a squash of:
    
    drm/amdkcl: test whether drm/drm_audio_component.h is available
    
    [why]
    drm_audio_component.h is not defined before drm version 4.19.0. So there
    will be build error when using them before drm version 4.19.0.
    
    [How]
    Use autoconf way to check drm_audio_component_ops,
    drm_audio_component_audio_ops,drm_audio_component in kernel. If they are
    not defined,avoid to use them in driver code.
    This autoconf patch is caused by patch:
    drm/amd/display: Add drm_audio_component support to amdgpu_dm
    
    Change-Id: Ic75928406a042a0e08f5b06cf64543555c62e476
    Signed-off-by: changzhu <Changfeng.Zhu@amd.com>
    Reviewed-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amdkcl: correct drm_audio_component_header.m4 patten
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Reviewed-by: Flora Cui <flora.cui@amd.com>
    Signed-off-by: Jack Gui <Jack.Gui@amd.com>
    
    drm/amdkcl: fix check for drm/drm_audio_component.h
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Reviewed-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Signed-off-by: Ma Jun <Jun.Ma2@amd.com>
    Change-Id: I7717d2f117361cfa3fa7a0f941e88ac3d99327b8

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.h
@@ -449,6 +449,7 @@ struct amdgpu_display_manager {
 	 */
 	struct mutex dc_lock;
 
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 	/**
 	 * @audio_lock:
 	 *
@@ -470,6 +471,7 @@ struct amdgpu_display_manager {
 	 * successfully, false otherwise.
 	 */
 	bool audio_registered;
+#endif
 
 	/**
 	 * @irq_handler_list_low_tab:
@@ -800,8 +802,10 @@ struct amdgpu_dm_connector {
 	 */
 	int max_vfreq ;
 
+#if defined(HAVE_DRM_AUDIO_COMPONENT_HEADER)
 	/* Audio instance - protected by audio_lock. */
 	int audio_inst;
+#endif
 
 	struct mutex hpd_lock;
 
