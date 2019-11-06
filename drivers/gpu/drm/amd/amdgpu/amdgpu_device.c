5a13ea988ada5089409d57f48c829f8c808dc576
Flora Cui <flora.cui@amd.com>
2019-11-06 10:07:49 +0800

    drm/amdkcl: add AMDKCL_ENABLE_RESIZE_FB_BAR for resize BAR enable
    This is a squash of:
    
    drm/amdkcl: move AMDKCL_ENABLE_RESIZE_FB_BAR to kcl_pci part
    no actual change.
        Reviewed-by: Guchun Chen <guchun.chen@amd.com>
        Signed-off-by: Flora Cui <flora.cui@amd.com>
    
    drm/amdkcl: rework pci resize dependency.
        Reviewed-by: Guchun Chen <guchun.chen@amd.com>
        Signed-off-by: Flora Cui <flora.cui@amd.com>
    
    drm/amdkcl: move pci resize macro to kcl_pci.h
        Signed-off-by: Flora Cui <flora.cui@amd.com>
    
    Signed-off-by: Flora Cui <flora.cui@amd.com>
    Acked-by: Feifei Xu <Feifei.Xu@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -1665,6 +1665,7 @@ void amdgpu_device_wb_free(struct amdgpu_device *adev, u32 wb)
 	spin_unlock_irqrestore(&adev->wb.lock, flags);
 }
 
+#ifdef AMDKCL_ENABLE_RESIZE_FB_BAR
 /**
  * amdgpu_device_resize_fb_bar - try to resize FB BAR
  *
@@ -1762,6 +1763,7 @@ int amdgpu_device_resize_fb_bar(struct amdgpu_device *adev)
 
 	return 0;
 }
+#endif
 
 /*
  * GPU helpers function.
