47913107967bcea5728244407bbbae99eb642d31
Benjamin Welton <bewelton@amd.com>
2024-10-09 21:32:55 -0700

    Add kfd_ioctl_profiler to contain profiler kernel driver changes
    kfd_ioctl_profiler takes a similar approach to that of
    kfd_ioctl_dbg_trap (which contains debugger related IOCTL
    services) where kfd_ioctl_profiler will contain all profiler
    related IOCTL services. The IOCTL is designed to be expanded
    as needed to support additional profiler functionality.
    
    The current functionality of the IOCTL is to allow for profilers
    which need PMC counters from GPU devices to both signal to other
    profilers that may be on the system that the device has active PMC
    profiling taking place on it (multiple PMC profilers on the same
    device can result in corrupted counter data) and to setup the device
    to allow for the collection of SQ PMC data on all queues on the device.
    
    For PMC data for the SQ block (such as SQ_WAVES) to be available
    to a profiler, mmPERFCOUNT_ENABLE must be set on the queues. When
    profiling a single process, the profiler can inject PM4 packets into
    each queue to turn on PERFCOUNT_ENABLE. When profiling system wide,
    the profiler does not have this option and must have a way to turn
    on profiling for queues in which it cannot inject packets into directly.
    
    Accomplishing this requires a few steps:
    
    1. Checking if the user has the necessary permissions to profile system
       wide on the device. This check uses the same check that linux perf
       uses to determine if a user has the necessary permissions to profile
       at this scope (primarily if the process has CAP_SYS_PERFMON or is root).
    
    2. Locking the device for profiling. This is done by setting a lock bit
       on the device struct and storing the process that locked the device.
    
    3. Iterating all queues on the device and issuing an MQD Update to enable
       perfcounting on the queues.
    
    4. Actions to cleanup if the process exits or releases the lock.
    
    The IOCTL also contains a link to the existing PC Sampling IOCTL as well.
    This is per a suggestion that we should potentially remove the PC Sampling
    IOCTL to have it be a part of the profiler IOCTL. This is a future change.
    In addition, we do expect to expand the profiler IOCTL to include
    additional profiler functionality in the future (which necessitates the
    use of a version number).
    
    Signed-off-by: Benjamin Welton <benjamin.welton@amd.com>
    Acked-by: Kent Russell <kent.russell@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_priv.h
+++ drivers/gpu/drm/amd/amdkfd/kfd_priv.h
@@ -437,6 +437,11 @@ struct kfd_dev {
 	int kfd_dev_lock;
 
 	atomic_t kfd_processes_count;
+
+	/* Lock for profiler process */
+	struct mutex profiler_lock;
+	/* Process currently holding the lock */
+	struct kfd_process *profiler_process;
 };
 
 struct kfd_ipc_obj;
@@ -620,6 +625,8 @@ enum mqd_update_flag {
 	UPDATE_FLAG_DBG_WA_ENABLE = 1,
 	UPDATE_FLAG_DBG_WA_DISABLE = 2,
 	UPDATE_FLAG_IS_GWS = 4, /* quirk for gfx9 IP */
+	UPDATE_FLAG_PERFCOUNT_ENABLE = 5,
+	UPDATE_FLAG_PERFCOUNT_DISABLE = 6,
 };
 
 struct mqd_update_info {
