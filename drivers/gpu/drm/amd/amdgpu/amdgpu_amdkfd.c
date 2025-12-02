ROCm/ROCK-Kernel-Driver

drm/amdkfd: add parameter force in kfd_process_evict_queues
It is to differenciate case scenario for proper behavior when
calling evict queues, such as GPU reset doesn't need to roll
back restoring partial evicted queues.

Signed-off-by: Eric Huang <jinhuieric.huang@amd.com>
Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>

drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd.c
@@ -190,7 +190,7 @@ void amdgpu_amdkfd_interrupt(struct amdgpu_device *adev,
 void amdgpu_amdkfd_suspend(struct amdgpu_device *adev, bool run_pm)
 {
 	if (adev->kfd.dev)
-		kgd2kfd_suspend(adev->kfd.dev, run_pm);
+		kgd2kfd_suspend(adev->kfd.dev, run_pm, true);
 }
 
 int amdgpu_amdkfd_resume(struct amdgpu_device *adev, bool run_pm, bool sync)

FireBurn/linux

drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd.c
@@ -255,7 +255,7 @@ void amdgpu_amdkfd_suspend(struct amdgpu_device *adev, bool suspend_proc)
 		if (adev->in_s0ix)
 			kgd2kfd_stop_sched_all_nodes(adev->kfd.dev);
 		else
-			kgd2kfd_suspend(adev->kfd.dev, suspend_proc);
+			kgd2kfd_suspend(adev->kfd.dev, suspend_proc, true);
 	}
 }
 
@@ -276,7 +276,7 @@ int amdgpu_amdkfd_resume(struct amdgpu_device *adev, bool resume_proc)
 void amdgpu_amdkfd_suspend_process(struct amdgpu_device *adev)
 {
 	if (adev->kfd.dev)
-		kgd2kfd_suspend_process(adev->kfd.dev);
+		kgd2kfd_suspend_process(adev->kfd.dev, true);
 }
 
 int amdgpu_amdkfd_resume_process(struct amdgpu_device *adev)