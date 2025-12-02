ROCm/ROCK-Kernel-Driver

drm/amdkcl: fake drm_gem_mmap for drm_gem_prime_mmap for legacy ps
This is caused by following commits:
71df0368e9b6 drm/amdgpu: Implement mmap as GEM object function
47d35c1c40d5 drm: Set vm_ops to GEM object's values during mmap

Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>

drivers/gpu/drm/ttm/ttm_bo_vm.c
@@ -597,6 +597,83 @@ static const struct vm_operations_struct ttm_bo_vm_ops = {
 	.access = ttm_bo_vm_access,
 };
 
+#ifdef HAVE_STRUCT_DRM_DRV_GEM_OPEN_OBJECT_CALLBACK
+static struct ttm_buffer_object *ttm_bo_vm_lookup(struct ttm_device *bdev,
+                                                  unsigned long offset,
+                                                  unsigned long pages)
+{
+        struct drm_vma_offset_node *node;
+        struct ttm_buffer_object *bo = NULL;
+
+        drm_vma_offset_lock_lookup(bdev->vma_manager);
+
+        node = drm_vma_offset_lookup_locked(bdev->vma_manager, offset, pages);
+        if (likely(node)) {
+                bo = container_of(node, struct ttm_buffer_object,
+                                  base.vma_node);
+                bo = ttm_bo_get_unless_zero(bo);
+        }
+
+        drm_vma_offset_unlock_lookup(bdev->vma_manager);
+
+        if (!bo)
+                pr_err("Could not find buffer object to map\n");
+
+        return bo;
+}
+
+static void ttm_bo_mmap_vma_setup(struct ttm_buffer_object *bo, struct vm_area_struct *vma)
+{
+        vma->vm_ops = &ttm_bo_vm_ops;
+
+        /*
+         * Note: We're transferring the bo reference to
+         * vma->vm_private_data here.
+         */
+
+        vma->vm_private_data = bo;
+
+        /*
+         * We'd like to use VM_PFNMAP on shared mappings, where
+         * (vma->vm_flags & VM_SHARED) != 0, for performance reasons,
+         * but for some reason VM_PFNMAP + x86 PAT + write-combine is very
+         * bad for performance. Until that has been sorted out, use
+         * VM_MIXEDMAP on all mappings. See freedesktop.org bug #75719
+         */
+        vma->vm_flags |= VM_MIXEDMAP;
+        vma->vm_flags |= (bo->ssg_can_map ? 0 : VM_IO) | VM_DONTEXPAND | VM_DONTDUMP;
+}
+
+int ttm_bo_mmap(struct file *filp, struct vm_area_struct *vma,
+                struct ttm_device *bdev)
+{
+        struct ttm_buffer_object *bo;
+        int ret;
+
+        if (unlikely(vma->vm_pgoff < DRM_FILE_PAGE_OFFSET_START))
+                return -EINVAL;
+
+        bo = ttm_bo_vm_lookup(bdev, vma->vm_pgoff, vma_pages(vma));
+        if (unlikely(!bo))
+                return -EINVAL;
+
+        if (unlikely(!bo->bdev->funcs->verify_access)) {
+                ret = -EPERM;
+                goto out_unref;
+        }
+        ret = bo->bdev->funcs->verify_access(bo, filp);
+        if (unlikely(ret != 0))
+                goto out_unref;
+
+        ttm_bo_mmap_vma_setup(bo, vma);
+        return 0;
+out_unref:
+        ttm_bo_put(bo);
+        return ret;
+}
+EXPORT_SYMBOL(ttm_bo_mmap);
+#endif /* HAVE_STRUCT_DRM_DRV_GEM_OPEN_OBJECT_CALLBACK */
+
 int ttm_bo_mmap_obj(struct vm_area_struct *vma, struct ttm_buffer_object *bo)
 {
 	/* Enforce no COW since would have really strange behavior with it. */

FireBurn/linux

drivers/gpu/drm/ttm/ttm_bo_vm.c
@@ -513,6 +513,83 @@ static const struct vm_operations_struct ttm_bo_vm_ops = {
 	.access = ttm_bo_vm_access,
 };
 
+#ifdef HAVE_STRUCT_DRM_DRV_GEM_OPEN_OBJECT_CALLBACK
+static struct ttm_buffer_object *ttm_bo_vm_lookup(struct ttm_device *bdev,
+                                                  unsigned long offset,
+                                                  unsigned long pages)
+{
+        struct drm_vma_offset_node *node;
+        struct ttm_buffer_object *bo = NULL;
+
+        drm_vma_offset_lock_lookup(bdev->vma_manager);
+
+        node = drm_vma_offset_lookup_locked(bdev->vma_manager, offset, pages);
+        if (likely(node)) {
+                bo = container_of(node, struct ttm_buffer_object,
+                                  base.vma_node);
+                bo = ttm_bo_get_unless_zero(bo);
+        }
+
+        drm_vma_offset_unlock_lookup(bdev->vma_manager);
+
+        if (!bo)
+                pr_err("Could not find buffer object to map\n");
+
+        return bo;
+}
+
+static void ttm_bo_mmap_vma_setup(struct ttm_buffer_object *bo, struct vm_area_struct *vma)
+{
+        vma->vm_ops = &ttm_bo_vm_ops;
+
+        /*
+         * Note: We're transferring the bo reference to
+         * vma->vm_private_data here.
+         */
+
+        vma->vm_private_data = bo;
+
+        /*
+         * We'd like to use VM_PFNMAP on shared mappings, where
+         * (vma->vm_flags & VM_SHARED) != 0, for performance reasons,
+         * but for some reason VM_PFNMAP + x86 PAT + write-combine is very
+         * bad for performance. Until that has been sorted out, use
+         * VM_MIXEDMAP on all mappings. See freedesktop.org bug #75719
+         */
+        vma->vm_flags |= VM_PFNMAP;
+        vma->vm_flags |= VM_IO | VM_DONTEXPAND | VM_DONTDUMP;
+}
+
+int ttm_bo_mmap(struct file *filp, struct vm_area_struct *vma,
+                struct ttm_device *bdev)
+{
+        struct ttm_buffer_object *bo;
+        int ret;
+
+        if (unlikely(vma->vm_pgoff < DRM_FILE_PAGE_OFFSET_START))
+                return -EINVAL;
+
+        bo = ttm_bo_vm_lookup(bdev, vma->vm_pgoff, vma_pages(vma));
+        if (unlikely(!bo))
+                return -EINVAL;
+
+        if (unlikely(!bo->bdev->funcs->verify_access)) {
+                ret = -EPERM;
+                goto out_unref;
+        }
+        ret = bo->bdev->funcs->verify_access(bo, filp);
+        if (unlikely(ret != 0))
+                goto out_unref;
+
+        ttm_bo_mmap_vma_setup(bo, vma);
+        return 0;
+out_unref:
+        ttm_bo_put(bo);
+        return ret;
+}
+EXPORT_SYMBOL(ttm_bo_mmap);
+#endif /* HAVE_STRUCT_DRM_DRV_GEM_OPEN_OBJECT_CALLBACK */
+
 /**
  * ttm_bo_mmap_obj - mmap memory backed by a ttm buffer object.
  *